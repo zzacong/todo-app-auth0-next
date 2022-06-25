134f7b14403361c9e7dd76f86545212312e49ead
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-25 20:47:57 +1000

    upgrade next, react, tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
