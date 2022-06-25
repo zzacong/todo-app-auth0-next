2b5a2ebf349ef52f73b48a6717e393cebd593cde
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-25 21:42:11 +1000

    set AUTH0_BASE_URL in .env.production

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    AUTH0_BASE_URL: process.env.VERCEL_URL || 'http://localhost:3000',
-  },
 }
 
 module.exports = nextConfig
